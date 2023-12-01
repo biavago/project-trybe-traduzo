2a893de71569109f54703ddc054c066400e6e1ac
Beatriz Vago <beatriz@Rafaels-MBP.Home>
2023-12-01 15:26:56 +0000

    Mais um teste implementado para a HistoryModel

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -6,3 +6,15 @@ def test_request_history():
     history_load = json.loads(HistoryModel.list_as_json())
 
     assert isinstance(history_load, list)
+
+    test_dict = {
+        "text_to_translate": "Hello, I like videogame",
+        "translate_from": "en",
+        "translate_to": "pt",
+    }
+
+    for i, li in enumerate([test_dict]):
+        assert '_id' in history_load[i]
+        assert li["text_to_translate"] == history_load[i]["text_to_translate"]
+        assert li["translate_from"] == history_load[i]["translate_from"]
+        assert li["translate_to"] == history_load[i]["translate_to"]
